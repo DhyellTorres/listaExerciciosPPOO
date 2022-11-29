6b47481842f0f2298fdd6146fabfc1fdbf70a7d9
Adryel do Nascimento Torres Silva <20192ewbj0329@belojardim.ifpe>
2022-11-28 21:31:58 -0300

    2 - Pesquisa na Web

--- websearch/Main.java
+++ websearch/Main.java
@@ -4,9 +4,12 @@ import java.io.File;
  * Launch the web-search example
  */
 public class Main {
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
         // Source file (in the project's data/ folder)
-        File inputTextFile = new File("./data/Hamlet.txt");
+        final File inputTextFile = new File("./data/Hamlet.txt");
 
         // Build object graph
         WebSearchModel model = new WebSearchModel(inputTextFile);
